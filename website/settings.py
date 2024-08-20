3a39dcaabbc87df4bc857c7b0eff5cee8e608ef4
krish <krishshah1411@gmail.com>
2024-08-20 17:34:00 -0400

    updated hosts for cloud hosting

--- website/settings.py
+++ website/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wr^q6^d$gv_=bmhqr*!%r1@1(mjtkqem4h6ex49ajl^kaeokvc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['krishshah.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','https://krishshah.pythonanywhere.com/']
 
 
 # Application definition
@@ -118,11 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'static'
 ]
 
 # Default primary key field type
